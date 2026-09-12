79b0964e12e28e313ef56924fc269aac45215efc
mohammad-moazen <www.m.h.moazen@gmail.com>
2026-09-12 16:19:43 +0330

    Export score-based PURE recommender helpers

--- src/pure_recommender/pure/__init__.py
+++ src/pure_recommender/pure/__init__.py
@@ -20,6 +20,13 @@ from .recommender import (
     build_pure_recommender_messages,
     pure_recommender_response_format,
 )
+from .recommender_scores import (
+    SCORE_MAX,
+    SCORE_MIN,
+    build_pure_recommender_score_messages,
+    parse_candidate_scores,
+    pure_recommender_score_response_format,
+)
 from .review_extractor import (
     RejectedEvidenceEntry,
     ReviewEvidenceEntry,
@@ -35,19 +42,24 @@ __all__ = [
     "RejectedEvidenceEntry",
     "ReviewEvidenceEntry",
     "ReviewExtraction",
+    "SCORE_MAX",
+    "SCORE_MIN",
     "UserProfile",
     "apply_retention_guard",
     "build_profile_id_map",
     "build_profile_updater_messages",
     "build_pure_recommender_messages",
+    "build_pure_recommender_score_messages",
     "build_review_extractor_messages",
     "concatenate_profile_and_extraction",
     "is_clear_lexical_overlap",
     "is_more_informative_overlap",
+    "parse_candidate_scores",
     "parse_profile_update",
     "parse_review_extraction",
     "profile_from_mapping",
     "profile_updater_response_format",
     "pure_recommender_response_format",
+    "pure_recommender_score_response_format",
     "review_extractor_response_format",
 ]
