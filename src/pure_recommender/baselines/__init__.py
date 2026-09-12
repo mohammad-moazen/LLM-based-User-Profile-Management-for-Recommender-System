17ab6930f1f80835c662dce807bec723dd99f930
mohammad-moazen <www.m.h.moazen@gmail.com>
2026-09-12 11:22:14 +0330

    feat: export ICL baseline builder

--- src/pure_recommender/baselines/__init__.py
+++ src/pure_recommender/baselines/__init__.py
@@ -1,5 +1,6 @@
 """LLM recommendation baselines reproduced from the PURE evaluation setup."""
 
+from .icl import build_icl_messages
 from .recency import build_recency_messages
 from .sequential import (
     SYSTEM_PROMPT,
@@ -12,6 +13,7 @@ __all__ = [
     "SYSTEM_PROMPT",
     "build_sequential_messages",
     "build_recency_messages",
+    "build_icl_messages",
     "parse_complete_ranking",
     "target_rank",
 ]
